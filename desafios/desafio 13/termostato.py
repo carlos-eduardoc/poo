188439fb04e4e4ce023b869749e1ee83aaaa8da1
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-04 08:17:55 -0300

    refactor: alterando funcionalidade de validação da temperatura

--- desafios/desafio 13/termostato.py
+++ desafios/desafio 13/termostato.py
@@ -11,7 +11,7 @@ class Termostato:
 
     @temperatura.setter
     def temperatura(self, valor):
-        if not valor % 1 == 0.5 or not valor > 16 or not valor < 30:
+        if valor % 0.5 != 0:
             raise ValueError('Temperatura invalida! Seu valor deve terminar inteiro ou com .5!')
         if valor < 16:
             self.__temperatura = 16
