ff3a56ec2520f9da43e93d4919c70004a24f13ea
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-04 21:41:53 -0300

    chore: alterei o __main__.py, para testar as novas funcionalidades do projeto

--- desafios/desafio 14/__main__.py
+++ desafios/desafio 14/__main__.py
@@ -3,9 +3,9 @@ from diariosecreto import DiarioSecreto
 
 def main():
     d1 = DiarioSecreto()
-    d1.escrever('OI a senha é linux é melhor que windows')
+    d1.escrever('OI a senha nao é linux é melhor que windows')
     d1.escrever('Eu gosto de fulano')
-    print(d1.ler('linux é melhor que windows'))
+    print(d1.ler())
     
 
 if __name__ == '__main__':
