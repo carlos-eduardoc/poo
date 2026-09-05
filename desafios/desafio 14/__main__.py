2939144c9fc8dec0c69ba2674a8523bd79395776
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-04 21:53:31 -0300

    chore: alteração dos testes para testar a estrutura de validação nova

--- desafios/desafio 14/__main__.py
+++ desafios/desafio 14/__main__.py
@@ -2,10 +2,22 @@ from diariosecreto import DiarioSecreto
 
 
 def main():
+    print('Digite a senha')
     d1 = DiarioSecreto()
     d1.escrever('OI a senha nao é linux é melhor que windows')
     d1.escrever('Eu gosto de fulano')
-    print(d1.ler())
+    
+    try:
+        print('Altere a senha')
+        d1.senha = '123carlos'
+        print('Senha alterada!')
+    except Exception as ex:
+        print(ex)
+    
+    try:
+        print(d1.ler())
+    except Exception as ex:
+        print(ex)
     
 
 if __name__ == '__main__':
