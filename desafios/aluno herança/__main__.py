d082453c22cab0470bd4f9f7568c7a72ae2ed55c
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-06 09:57:44 -0300

    refactor: melhorei a estrutura do fluxo contendo try/except e testei novas funcionalidades adicionadas em aluno.py

--- desafios/aluno herança/__main__.py
+++ desafios/aluno herança/__main__.py
@@ -5,13 +5,28 @@ from rich import inspect
 
 def main():
     aluno = Aluno('Carlos', 1995)
-    aluno.curso = 'ENG'
-    aluno.add_curso('MODAU')
-    inspect(aluno, private=True, methods=True)
+    try:
+        aluno.curso = 'ENG'
+    except Exception as ex:
+        print(ex)
     
-    aluno.curso = 'MODAU'
+    try:
+        aluno.add_curso('MODAU')
+    except Exception as ex:
+        print(ex)
     
+    try:    
+        aluno.curso = 'MODAU'
+    except Exception as ex:
+        print(ex)
+        
+    try:
+        aluno.add_curso('ADS')
+    except Exception as ex:
+        print(ex)
+        
     inspect(aluno, private=True, methods=True)
+
 
 if __name__ == '__main__':
     main()
