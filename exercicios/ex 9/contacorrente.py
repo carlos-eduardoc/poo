47133ae8fdb1e4408d4138836e82e236d6a4da4a
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-09 20:34:27 -0300

    refactor: usei polimorfismo de inclusao(override) no metodo de validação do saque, adaptando para o desconto da taxa + valor, tambem modifiquei o saque() reduzi o numero de linhas aplicando esta nova validação melhorando a estrutura do codigo

--- exercicios/ex 9/contacorrente.py
+++ exercicios/ex 9/contacorrente.py
@@ -7,13 +7,20 @@ class ContaCorrente(Conta):
     def __init__(self, saldo):
         super().__init__(saldo)
     
+    
+    def validador_saque(self, valor):
+        saldo_apos_contas = self._saldo - ContaCorrente.taxa_fixa - valor
+        if valor < 1:
+            raise ValueError('Não foi possivel realizar o saque, o valor é menor que R$1,00')
+        elif saldo_apos_contas > self._saldo:
+            raise ValueError('Não foi possivel realizar o saque, valor + taxa é maior que seu saldo!')
+        else:
+            return True
+    
+    
     def saque(self, valor):
         validacao = self.validador_saque(valor)
         if validacao:
-            saldo_apos_contas = self._saldo - ContaCorrente.taxa_fixa - valor 
-            if saldo_apos_contas > 0:  
-                self._saldo -= ContaCorrente.taxa_fixa
-                self._saldo -= valor
-                print(f'Saque realizado! -{valor + ContaCorrente.taxa_fixa:,.2f}')
-            else:
-                raise ValueError('Saldo invalido para o saque!')
+            self._saldo -= ContaCorrente.taxa_fixa
+            self._saldo -= valor
+            print(f'Saque realizado! -R${valor - ContaCorrente.taxa_fixa:,.2f}')
