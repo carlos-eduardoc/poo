65a972426235cd940f7798f3745be0c8e4b64b98
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-09 10:06:35 -0300

    feat: mudei da linha 12 até a 19, para resolver o problema do saldo negativo criei uma variavel para calcular antes o valor da conta, se fosse postivo o saque é efetuado descontando taxa e valor de saldo e imprime uma mensagem de efetivação, caso ao contrario ele interrompe o fluxo e imprime que o saque é invalido

--- exercicios/ex 9/contacorrente.py
+++ exercicios/ex 9/contacorrente.py
@@ -9,6 +9,11 @@ class ContaCorrente(Conta):
     
     def saque(self, valor):
         validacao = self.validador_saque(valor)
-        if validacao == True:
-            self._saldo -= valor
-            self._saldo -= ContaCorrente.taxa_fixa
+        if validacao:
+            saldo_apos_contas = self._saldo - ContaCorrente.taxa_fixa - valor 
+            if saldo_apos_contas > 0:  
+                self._saldo -= ContaCorrente.taxa_fixa
+                self._saldo -= valor
+                print(f'Saque realizado! -{valor + ContaCorrente.taxa_fixa:,.2f}')
+            else:
+                raise ValueError('Saldo invalido para o saque!')
