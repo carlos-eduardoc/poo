0c0c23ebe3130fc2085b0ccf97903a6ee63b7a31
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-09 09:46:12 -0300

    fix: limpando cache do projeto

--- desafios/desafio 10/__main__.py
+++
@@ -1,9 +0,0 @@
-from bebidaquente import *
-
-def main():
-    b = Leite()
-    b.preparar()
-
-
-if __name__ == "__main__":
-    main()
--- rich/rich004.py
+++
@@ -1,9 +0,0 @@
-# inspect  ele te entrega o manual de uma classe e objeto, usando inspect(obj)
-# usasse o all=True para mostra todo o conteudo
-
-from rich import inspect
-from rich import print
-
-print(int)
-# olha a diferença
-inspect(int)
