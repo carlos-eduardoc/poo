d7cd3548ec384d749f92c7436a7146f0bcd5d08d
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-11 09:39:42 -0300

    feat: criação de atributos x e y, adição de metodos magicos para interagir com o objeto, add, sub e mul

--- exercicios/ex12/vetor2d.py
+++ exercicios/ex12/vetor2d.py
@@ -1,14 +1,25 @@
 class Vetor2D:
-    def __init__(self):
-        ...
-    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
     
     def __add__(self, outro):
-        ...
+        novo_x = self.x + outro.x
+        novo_y = self.y + outro.y
+        vetor_novo = Vetor2D(novo_x, novo_y)
+        return vetor_novo
     
     
     def __sub__(self, outro):
-        ...
+        novo_x = self.x - outro.x
+        novo_y = self.y - outro.y
+        vetor_novo = Vetor2D(novo_x, novo_y)
+        return vetor_novo
     
-    def __mul__(outro):
-        ...
+    
+    def __mul__(self, valor):
+        novo_x = self.x * valor
+        novo_y = self.y * valor
+        vetor_novo = Vetor2D(novo_x, novo_y)
+        return vetor_novo
