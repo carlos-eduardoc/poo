b98324bdfd0048c4a6a349b58478763a213363d9
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-11 09:22:05 -0300

    init: inicialização e construção do esqueleto de vetor2d.py

---
+++ exercicios/ex12/vetor2d.py
@@ -0,0 +1,14 @@
+class Vetor2D:
+    def __init__(self):
+        ...
+    
+    
+    def __add__(self, outro):
+        ...
+    
+    
+    def __sub__(self, outro):
+        ...
+    
+    def __mul__(outro):
+        ...
