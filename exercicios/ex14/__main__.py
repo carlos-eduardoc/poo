f2943714549346dfffb2d8991caecc9a1943c2a1
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-12 08:31:00 -0300

    init: inicializando as estruturas, em fracao.py defini os atributos e adicionei a estrutura dos metodos, em __main__.py criei o fluxo base paraexecução

---
+++ exercicios/ex14/__main__.py
@@ -0,0 +1,8 @@
+from fracao import Fracao
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
---
+++ exercicios/ex14/fracao.py
@@ -0,0 +1,12 @@
+class Fracao:
+    def __init__(self, numerador, denominador):
+        self.numerador = numerador
+        self.denominador = denominador
+        
+    
+    def __str__(self):
+        pass
+
+    
+    def __add__(self, outro):
+       pass
