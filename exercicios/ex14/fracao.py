24f29d0cd606972044cb3761bccb4345ed398237
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-12 08:54:18 -0300

    feat: sobrescrevi str e add, str ele retorna a fração do objeto, e add ele soma as frações com regras e trasnforma em uma nova fração assim retornando-a

--- exercicios/ex14/fracao.py
+++ exercicios/ex14/fracao.py
@@ -5,8 +5,17 @@ class Fracao:
         
     
     def __str__(self):
-        pass
+        return f'{self.numerador}/{self.denominador}'
 
     
-    def __add__(self, outro):
-       pass
+    def __add__(self, outro):            
+        if self.denominador == outro.denominador:
+            novo_n = self.numerador + outro.numerador
+            novo_d = self.denominador
+        else:
+            novo_n = self.numerador * outro.denominador + outro.numerador * self.denominador
+            novo_d = self.denominador * outro.denominador
+       
+        nova_fracao = Fracao(novo_n, novo_d)
+        return nova_fracao
+           
