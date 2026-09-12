e0bb3870616ec29e8c556478a5dd2e502e8b3a33
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-12 10:04:57 -0300

    feat: criei o objeto c1, entao testei metodo dobrar com diferentes comportamentos, utilizei print e o center() para melhor entendimento no terminal

--- exercicios/ex16/__main__.py
+++ exercicios/ex16/__main__.py
@@ -1,7 +1,29 @@
 from calculadora import Calculadora
 
 def main():
-    ...
+    c1 = Calculadora()
+    
+    print('Dobrar: INT'.center(20))
+    print('-' * 20)  
+    print(f'{c1.dobrar(10)}'.center(17))
+    
+    print('\n')
+    
+    print('Dobrar: FLOAT'.center(20))
+    print('-' * 20)  
+    print(f'{c1.dobrar(2.5)}'.center(20))
+    
+    print('\n')
+    
+    print('Dobrar: STR'.center(20))
+    print('-' * 20)  
+    print(c1.dobrar('pooEMpython').center(20))
+    
+    print('\n')
+    
+    print('Dobrar: LIST'.center(20))
+    print('-' * 20)  
+    print(f'{c1.dobrar([1, 2, 3])}'.center(20))
     
     
 if __name__ == '__main__':
