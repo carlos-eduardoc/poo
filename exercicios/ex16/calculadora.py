5092e9db8b8eefe8764f4fc1c2d7917f750a27bc
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-12 10:03:20 -0300

    feat: adicionei os metodos de sobrecarga dos tipos int, float, str, list que sao derivados de dobrar, em que cada um desses dobrem só que com comportamentos diferentes

--- exercicios/ex16/calculadora.py
+++ exercicios/ex16/calculadora.py
@@ -5,4 +5,18 @@ class Calculadora:
     def dobrar(self, valor):
         return f'Valor desconhecido: {valor}'
     
+    @dobrar.register(int)
+    def _(self, valor):
+        return valor * 2
     
+    @dobrar.register(float)
+    def _(self, valor):
+        return valor * 2
+    
+    @dobrar.register(str)
+    def _(self, valor):
+        return valor * 2
+    
+    @dobrar.register(list)
+    def _(self, valor):
+        return valor * 2
