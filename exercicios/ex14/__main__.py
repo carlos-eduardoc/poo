6c4aeb78ee5377add5f774f0764f93072c557eb7
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-12 08:56:09 -0300

    feat: criação dos objetos da classe Fracao, utilizando o dunder method str mostrando a fração e o seu decimal, depois testamos o add assim somando uma fração com outra e por fim combinando com o str

--- exercicios/ex14/__main__.py
+++ exercicios/ex14/__main__.py
@@ -1,7 +1,15 @@
 from fracao import Fracao
 
+
 def main():
-    ...
+    f1 = Fracao(3, 7)
+    f2 = Fracao(4, 7)
+
+    print(f'Fração: {f1}  Decimal: {f1.numerador/f1.denominador:.2f}')
+    print(f'Fração: {f2}  Decimal: {f2.numerador/f2.denominador:.2f}')
+
+    f3 = f1 + f2
+    print(f'Fração: {f3}  Decimal: {f3.numerador/f3.denominador:.2f}')
 
 
 if __name__ == '__main__':
