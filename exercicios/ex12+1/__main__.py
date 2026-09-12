d2e404a416c48740b3ee645e9860d29901cad9b6
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-12 08:04:41 -0300

    feat: criação de dois objetos de retangulo com suas medidas, e execução para testar os metodos magicos lt e gt utilizando prints para melhor visual

--- exercicios/ex12+1/__main__.py
+++ exercicios/ex12+1/__main__.py
@@ -1,8 +1,16 @@
 from retangulo import Retangulo
 
 def main():
-    ...
-
+    retangulo_1 = Retangulo(4, 5)
+    retangulo_2 = Retangulo(3, 7)
+    
+    
+    print('retangulo 1 é maior que retangulo 2?')
+    print(f'R:{retangulo_1 > retangulo_2}'.center(31))
+    
+    print('retangulo 1 é menor que retangulo 2?')
+    print(f'R:{retangulo_1 < retangulo_2}'.center(30))
+    
 
 if __name__ == '__main__':
     main()
