f20fc47eafc036b1e8a92e72d33cf9ff617ce354
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-12 09:30:05 -0300

    feat: criei o objeto f1, e testei todas as funcionalidades de formatação

--- exercicios/ex15/__main__.py
+++ exercicios/ex15/__main__.py
@@ -1,8 +1,31 @@
 from formatador import Formatador
 
 def main():
-    ...
+    f1 = Formatador()
     
+    print('Formatação INT'.center(30))
+    print('-' * 30)
+    print(f1.formatar(1000).center(30))
+    
+    print('\n')
+    
+    print('Formatação STR'.center(30))
+    print('-' * 30)
+    print(f1.formatar('oi me segue no github e de uma estrela:)').center(20))
+    
+    print('\n')
+        
+    print('Formatação LIST'.center(30))
+    print('-' * 30)
+    print(f1.formatar([1, 'oi', 99, 2.9]).center(30))
+    
+    print('\n')
+        
+    print('Formatação DESCONHECIDA'.center(30))
+    print('-' * 30)
+    print(f1.formatar(2.9).center(30))
+        
+        
     
 if __name__ == '__main__':
     main()
