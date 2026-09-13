678071e5681c4f446287dd0d572900b8672a3775
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-13 09:46:02 -0300

    init: importação dos arquivos a ser executados, e definição da estrutura principal do __main__

---
+++ exercicios/ex17/__main__.py
@@ -0,0 +1,12 @@
+from ducktyping import ativar_dispositivo
+from motor import Motor
+from lampada import Lampada
+from radio import Radio
+
+
+def main():
+    ...
+    
+
+if __name__ == '__main__':
+    main()
