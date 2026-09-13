88a5b9a758ec308190a0766f1aea6aecc36da2f3
carlos-eduardoc <menino.podsonhar@gmail.com>
2026-09-13 09:52:35 -0300

    refactor: alterei o ligar do print para após as exceções para mostrar o status mesmo se der erro

--- exercicios/ex17/ducktyping.py
+++ exercicios/ex17/ducktyping.py
@@ -1,6 +1,6 @@
 def ativar_dispositivo(obj):
     try:
-        obj.ligar()
-        print(obj)
+        print(obj.ligar())
     except (AttributeError, TypeError) as ex:
         print(f'{ex}: O objeto não tem o metodo necesario(ligar())')
+    print(obj)
